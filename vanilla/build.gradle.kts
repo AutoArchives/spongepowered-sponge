abd5b999c1efd4e5584f68cd9e5788483b3c1230
Yeregorix <yeregorixcraft@gmail.com>
2025-04-18 21:26:09 +0200

    No longer shade configurate-jackson

--- vanilla/build.gradle.kts
+++ vanilla/build.gradle.kts
@@ -221,10 +221,6 @@ dependencies {
         exclude(group = "org.spongepowered", module = "configurate-core")
         exclude(group = "org.checkerframework", module = "checker-qual")
     }
-    boot(libs.configurate.jackson) {
-        exclude(group = "org.spongepowered", module = "configurate-core")
-        exclude(group = "org.checkerframework", module = "checker-qual")
-    }
 
     boot(libs.mixin)
     boot(libs.mixinextras.common)
