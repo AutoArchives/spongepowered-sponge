e6ea5b7132dd80a36719195dc12b7f66d2a1100d
Gabriel Harris-Rouquette <gabizou@me.com>
2026-04-12 19:29:09 -0700

    fix(data): DyeColor bridge for ColorCollection blocks
    Vanilla 26.2 introduced ColorCollection to bulk-register
    dye-colored blocks using mapColor(MapColor) instead of
    mapColor(DyeColor). The existing PropertiesMixin hook
    never fires, leaving DyeColorBlockBridge unpopulated.
    ColorCollectionMixin injects at registerBlocks() RETURN
    to stamp the bridge on all 16 blocks per collection.
    
    Signed-off-by: Gabriel Harris-Rouquette <gabizou@me.com>

---
+++ src/mixins/java/org/spongepowered/common/mixin/core/world/level/block/ColorCollectionMixin.java
@@ -0,0 +1,58 @@
+/*
+ * This file is part of Sponge, licensed under the MIT License (MIT).
+ *
+ * Copyright (c) SpongePowered <https://www.spongepowered.org>
+ * Copyright (c) contributors
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package org.spongepowered.common.mixin.core.world.level.block;
+
+import net.minecraft.world.item.DyeColor;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.ColorCollection;
+import net.minecraft.world.level.block.state.BlockBehaviour;
+import org.apache.commons.lang3.function.TriFunction;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.common.bridge.block.DyeColorBlockBridge;
+
+import java.util.function.BiFunction;
+import java.util.function.Function;
+
+@Mixin(ColorCollection.class)
+public abstract class ColorCollectionMixin {
+
+    @Inject(method = "registerBlocks", at = @At("RETURN"))
+    private static <T extends Block> void impl$stampDyeColor(
+        final String id,
+        final TriFunction<String, Function<BlockBehaviour.Properties, Block>, BlockBehaviour.Properties, Block> register,
+        final BiFunction<DyeColor, BlockBehaviour.Properties, T> colorBlockFactory,
+        final Function<DyeColor, BlockBehaviour.Properties> propertiesSupplier,
+        final CallbackInfoReturnable<ColorCollection<Block>> cir
+    ) {
+        final ColorCollection<Block> collection = cir.getReturnValue();
+        for (final DyeColor color : DyeColor.values()) {
+            ((DyeColorBlockBridge) collection.pick(color))
+                .bridge$setDyeColor((org.spongepowered.api.data.type.DyeColor) (Object) color);
+        }
+    }
+}
